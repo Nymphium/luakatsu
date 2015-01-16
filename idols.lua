14d88d5949b90787c728612dbc3cd08b1aa57399
nymphium <s1311350@coins.tsukuba.ac.jp>
2015-01-17 05:01:23 +0900

    move idols into idols.lua

---
+++ idols.lua
@@ -0,0 +1,195 @@
+return {
+	{
+		name = "星宮 いちご",
+		actor = "諸星 すみれ",
+		birthday = "3/15",
+		zodiac_sign = "Picces",
+		blood_type = "O",
+		favorite_brand = "Angely Sugar",
+		signature_songs = {"輝きのエチュード", "Growing for a dream", "Dance in the rain"},
+		belonging_to = {"Soleil", "STAR☆ANIS", "2wingS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "霧矢 あおい",
+		actor = "田所 あずさ",
+		birthday = "1/31",
+		zodiac_sign = "Aquarius",
+		blood_type = "A",
+		favorite_brand = "FUTURING GIRL",
+		signature_songs = {"prism spiral", "Stranger alien"},
+		belonging_to = {"Soleil", "STAR☆ANIS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "紫吹 蘭",
+		actor = "大橋 彩香",
+		birthday = "8/3",
+		zodiac_sign = "Leo",
+		blood_type = "AB",
+		favorite_brand = "SPICY AGEHA",
+		signature_songs = {"Trap of Love"},
+		belonging_to = {"Soleil", "Tristar", "STAR☆ANIS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "神谷 しおん",
+		actor = "瀬戸 麻沙美",
+		birthday = "1/11",
+		zodiac_sign = "Capricorn",
+		blood_type = "A",
+		favorite_brand = "FUTURING GIRL",
+		signature_songs ={},
+		belonging_to = {"ぽわぽわプリリン"},
+		school = "スターライト学園",
+	},
+	{
+		name = "有栖川 おとめ",
+		actor = "黒沢 ともよ",
+		birthday = "5/5",
+		zodiac_sign = "Taurus",
+		blood_type = "B",
+		favorite_brand = "HAPPY RAINBOW",
+		signature_songs = {"Angel Snow", "放課後ポニーテール", "CHU-CHU RAINBOW"},
+		belonging_to = {"ぽわぽわプリリン", "STAR☆ANIS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "藤堂 ユリカ",
+		actor = "沼倉 愛美",
+		birthday = "12/26",
+		zodiac_sign = "Capricorn",
+		blood_type = "B",
+		favorite_brand = "LoLi GoThiC",
+		signature_songs = {"硝子ドール", "永遠の灯"},
+		belonging_to = {"Tristar", "STAR☆ANIS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "北大路 さくら",
+		actor = "安野 希世乃",
+		birthday = "4/6",
+		zodiac_sign = "Aries",
+		blood_type = "A",
+		favorite_brand = "Aurora Fantasy",
+		signature_songs = {"同じ地球のしあわせに"},
+		belonging_to = {"ぽわぽわプリリン", "STAR☆ANIS"},
+		school = "スターライト学園",
+		pet = {"koiichi", "koini", "koisan", "koishi", "koigo", "koiroku"}
+	},
+	{
+		name = "一ノ瀬 かえで",
+		actor = "三村 ゆうな",
+		birthday = "11/23",
+		zodiac_sign = "Sagittarius",
+		blood_type = "O",
+		favorite_brand = "MAGICAL TOY",
+		signature_songs = {},
+		belonging_to = {"Tristar", "STAR☆ANIS"},
+		school = "スターライト学園"
+	},
+	{
+		name = "大空 あかり",
+		actor = "下地 柴野",
+		birthday = "4/1",
+		zodiac_sign = "Aries",
+		blood_type = "A",
+		favorite_brand = "Dreamy Crown",
+		signature_songs = {},
+		belonging_to = {"GOGO! いちご応援隊"},
+		school = "スターライト学園"
+	},
+	{
+		name = "氷上 スミレ",
+		actor = "和久井 優",
+		birthday = "10/20",
+		zodiac_sign = "Libra",
+		blood_type = "AB",
+		favorite_brand = "LoLi GoThiC",
+		signature_songs = {"タルト･タタン"},
+		belonging_to = {"GOGO! いちご応援隊"},
+		school = "スターライト学園"
+	},
+	{
+		name = "新条 ひなき",
+		actor = "石川 由依",
+		birthday = "6/11",
+		zodiac_sign = "Gemini",
+		favorite_brand = "ViVid Kiss",
+		signature_songs = {},
+		belonging_to = {"GOGO! いちご応援隊"},
+		school = "スターライト学園"
+	},
+
+	-- Dream Academy
+	{
+		name = "音城 セイラ",
+		actor = "石原 夏織",
+		birthday = "11/11",
+		zodiac_sign = "Scorpio",
+		blood_type = "A",
+		favorite_brand = "Swing ROCK",
+		signature_songs = {},
+		belonging_to = {"2wingS"},
+		schoo = "ドリーム・アカデミー"
+	},
+	{
+		name = "冴草 きい",
+		actor = "秋奈",
+		birthday = "12/3",
+		zodiac_sign = "Sagittarius",
+		blood_type = "O",
+		favorite_brand = "MAGICAL TOY",
+		signature_songs = {"マジカルタイム"},
+		belonging_to = {},
+		schoo = "ドリーム・アカデミー"
+	},
+	{
+		name = "風沢 そら",
+		actor = "高橋 未奈美",
+		birthday = "10/2",
+		zodiac_sign = "Libra",
+		blood_type = "B",
+		favorite_brand = "Bohemian Sky",
+		signature_songs = {"Kira･pata･shining"},
+		belonging_to = {},
+		schoo = "ドリーム・アカデミー",
+		pet = "パーム"
+	},
+	{
+		name = "姫里 マリア",
+		actor = "冨岡 美沙子",
+		birthday = "4/18",
+		zodiac_sign = "Virgo",
+		blood_type = "O",
+		favorite_brand = "Aurora Fantasy",
+		signature_songs = {"オーロラプリンセス"},
+		belonging_to = {},
+		schoo = "ドリーム・アカデミー",
+		pet = "エリザベス三世"
+	},
+
+	-- other
+	{
+		name = "神崎 美月",
+		actor = "寿 美菜子",
+		birthday = "9/18",
+		zodiac_sign = "Virgo",
+		blood_type = "A",
+		favorite_brand = "LOVE MOONRISE",
+		signature_songs = {"Nove on now!", "Moonlight destiny", "Precious"},
+		belonging_to = {"Tristar", "STAR☆ANIS", "WM"},
+	},
+	{
+		name = "夏樹 みくる",
+		actor = "洲崎 綾",
+		birthday = "7/7",
+		zodiac_sign = "Cancer",
+		blood_type = "O",
+		favorite_brand = "ViVid Kiss",
+		signature_songs = {"オトナモード"},
+		belonging_to = {"WM"},
+		pet = "フェレッ太"
+	}
+}
+
