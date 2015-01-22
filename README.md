8d43f536a115a2c08307690408ed658acfc1913c
nymphium <s1311350@coins.tsukuba.ac.jp>
2015-01-22 13:53:18 +0900

    大幅うっぴでーと

--- README.md
+++ README.md
@@ -20,14 +20,30 @@ $ lua
 table
 ```
 
-### name
+### profile
 
 ```lua
 print(Aikatsu.ichigo) ---> 星宮 いちご
+
+Aikatsu.ichigo()
+
+--[[
+name	星宮 いちご
+actor	諸星 すみれ
+birtyday	3/15
+zodiac_sign	Picces
+blood_type	O
+favorite_brand	Angely Sugar
+signature_songs	輝きのエチュード, Growing for a dream, Dance in the rain
+sing	霧島 若歌
+belonging_to	Soleil,STAR☆ANIS,2wingS
+school	スターライト学園
+--]]
+
+print(table.concat(Aikastu.ichigo.signature_songs), ", ") ---> 輝きのエチュード, Growing for a dream, Dance in the rain
 ```
 
-
-### oneliner
+### one-liner
 ```sh
 $ lua -luakatsu -e "print(Aikatsu())"
 私のアツいアイドル活動､アイカツ! 始まります! ﾌﾌｯﾋ
--- luakatsu/bin/char_setmeta.lua
+++ luakatsu/bin/char_setmeta.lua
@@ -1,14 +1,39 @@
 return function(t)
-	local index = setmetatable({}, {__mode = "k"})
+	local index = {}
 
 	for _, i in pairs(t) do
 		index[i.name_rm] = i
 	end
 
-	for k in pairs(t) do
-		t[k].name_rm = nil
+	-- local function prof(chara)
+		-- for i, j in pairs(chara) do
+			-- if type(j) == "table" then
+				-- j = table.concat(j, ", ")
+			-- end
+
+			-- if i and j then
+				-- print(i, j)
+			-- end
+		-- end
+	-- end
+
+	local ret = {}
+
+	for i, j in pairs(t) do
+		t[i].name_rm = nil
+		-- table.insert(ret, setmetatable(j, {__call = prof(j)}))
+		table.insert(ret, setmetatable(j, {__call = function()
+			for k, l in pairs(j) do
+				if type(l) == "table" then
+					l = table.concat(l, ", ")
+				end
+				if k and l then
+					print(k, l)
+				end
+			end
+		end}))
 	end
 
-	return setmetatable(t, {__index = index})
+	return setmetatable(ret, {__index = index})
 end
 
