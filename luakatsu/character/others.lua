adffb6c65d28b2e7d8b9a845b6be415883b71972
nymphium <s1311350@coins.tsukuba.ac.jp>
2015-02-01 10:10:28 +0900

    revise characters' database

--- luakatsu/character/others.lua
+++ luakatsu/character/others.lua
@@ -1,27 +1,33 @@
+local type
+local cute, cool, sexy, pop = "Cute", "Cool", "Sexy", "Pop"
+
+
 return {
 	{
 		name = "星宮 りんご",
-		name_rm = "ringo",
+		name_rm = "Ringo",
 		actor = "能登 麻美子",
+		type = sexy,
 		signature_songs = {"wake up my music"},
 		sing = "成瀬 瑛美",
 		belonging_to = {"マスカレード"},
 	},
 	{
 		name = "星宮 太一",
-		name_rm = "taichi",
+		name_rm = "Taichi",
 		actor = "子安 武人",
 	},
 	{
 		name = "星宮 らいち",
-		name_rm = "raichi",
+		name_rm = "Raichi",
 		actor = "瀬戸 麻沙美",
 
 	},
 	{
 		name = "光石 織姫",
-		name_rm = "orihime",
+		name_rm = "Orihime",
 		actor = "松谷 彼哉",
+		type = sexy,
 		signature_songs = {"wake up my music"},
 		sing = "相沢 梨沙",
 		belonging_to = {"マスカレード"},
@@ -29,13 +35,13 @@ return {
 	},
 	{
 		name = "ジョニー 別府",
-		name_rm = "johnny",
+		name_rm = "Johnny",
 		actor = "保村 真",
 		school = "スターライト学園"
 	},
 	{
 		name = "鈴川 直人",
-		name_rm = "nao",
+		name_rm = "Nao",
 		actor = "豊永 利行",
 		signature_songs = {"アリスブルーのキス"},
 		belonging_to = {"More Than True"},
@@ -43,21 +49,21 @@ return {
 	},
 	{
 		name = "ヒロ",
-		name_rm = "hiro",
+		name_rm = "Hiro",
 		actor = "岸尾 大輔",
 		signature_songs = {"アリスブルーのキス"},
 		belonging_to = {"More Than True"}
 	},
 	{
 		name = "シュラト",
-		name_rm = "shurato",
+		name_rm = "Shurato",
 		actor = "野島 裕史",
 		signature_songs = {"アリスブルーのキス"},
 		belonging_to = {"More Than True"}
 	},
 	{
 		name = "キング",
-		name_rm = "king",
+		name_rm = "King",
 		actor = "日野聡",
 		signature_songs = {"アリスブルーのキス"},
 		belonging_to = {"More Than True"}
