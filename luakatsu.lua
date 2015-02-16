af6918ac03a09563684e61f914c62fb90f755983
nymphium <s1311350@coins.tsukuba.ac.jp>
2015-02-16 19:49:04 +0900

    update

--- luakatsu.lua
+++ luakatsu.lua
@@ -1,18 +1,8 @@
 local char_setmeta = require "luakatsu.bin.char_setmeta"
-
 local idols = char_setmeta(require("luakatsu.lib.characters.idols"))
 local others = char_setmeta(require("luakatsu.lib.characters.others"))
-
 local t = {idols = idols, others = others}
-
 local main_characters = {}
-
-for _, h in pairs(t) do
-	for _, i in pairs(h) do
-		table.insert(main_characters, i)
-	end
-end
-
 local _M = getmetatable(idols).__index
 local m2 = getmetatable(others).__index
 
@@ -21,9 +11,6 @@ for key, i in pairs(m2) do
 
 	_M[key] = i
 end
-
-_M.idols = idols
-_M.others = others
 
 Aikatsu = setmetatable(main_characters, {__index = _M, __call = function() print("私のアツいアイドル活動､アイカツ! 始まります! ﾌﾌｯﾋ") end})
 
--- luakatsu/bin/char_setmeta.lua
+++ luakatsu/bin/char_setmeta.lua
@@ -1,42 +1,42 @@
+local index = {}
+local ret = {}
+
+local prof_iter = function(prof)
+	local pos = 0
+
+	return function()
+		pos = pos + 1
+
+		if not prof[pos] then return nil end
+
+		return prof[pos][1], prof[pos][2]
+	end
+end
+
+local printprof = function(prof)
+	for key, val in prof_iter(prof) do
+		if type(val) == "table" then
+			val = table.concat(val, ", ")
+		end
+
+		if key and val then
+			print(key, val)
+		end
+	end
+end
+
+local mkprofmt = function(prof)
+	local mt = {}
+
+	for key, val in prof_iter(prof) do
+		mt[key] = val
+	end
+
+	return mt
+end
+
+
 return function(t)
-	local index = {}
-	local ret = {}
-
-	local function prof_iter(prof)
-		local pos = 0
-
-		return function()
-			pos = pos + 1
-
-			if not prof[pos] then return nil end
-
-			return prof[pos][1], prof[pos][2]
-		end
-	end
-
-	local function printprof(prof)
-		for key, val in prof_iter(prof) do
-			if type(val) == "table" then
-				val = table.concat(val, ", ")
-			end
-
-			if key and val then
-				print(key, val)
-			end
-		end
-	end
-
-	local function mkprofmt(prof)
-		local mt = {}
-
-		for key, val in prof_iter(prof) do
-			mt[key] = val
-		end
-
-		return mt
-	end
-
-
 	for _, chara in pairs(t) do
 		index[chara.name_rm] = chara
 		chara.name_rm = nil
